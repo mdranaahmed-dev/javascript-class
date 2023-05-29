efbc0d8bb3f7bf8a25a958b0fb2d45c308f50ce5
mdranaahmed-dev <mdranaahmed.dev>
2023-05-29 10:45:54 -0600

    string

--- class-15/Test.js
+++ class-15/Test.js
@@ -1,3 +1,20 @@
-function displayDate() {
-    document.getElementById("demo2").innerHTML = Date();
-  }
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+document.getElementById("demo").innerHTML = text.length;
+console.log(text.length);
+
+let rana = "We are the so-called \"Vikings\" from the north.";
+console.log(rana);
+
+let y = new String("John");
+let x = "John";
+console.log(y == x);
+
+let a = new String("John");
+let b = "John";
+console.log(a === b);
+
+let d = new String("John");
+let f = new String("John");
+
+document.getElementById("demo").innerHTML = (d == f);
