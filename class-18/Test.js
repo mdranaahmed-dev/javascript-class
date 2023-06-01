815ed351a57565d0f4b90bc733cf6440646d1e33
mdranaahmed-dev <mdranaahmed.dev>
2023-06-01 11:19:01 -0600

    template-literals

---
+++ class-18/Test.js
@@ -0,0 +1,17 @@
+let country = `Bangladesh`;
+
+let a = 17;
+let b = 1;
+
+let sentence = `I love ${country} it's ${a+b}`;
+console.log(sentence);
+document.getElementById("demo2").innerHTML= sentence;
+
+let text =
+
+`The quick
+brown fox
+jumps over
+the lazy dog`;
+
+document.getElementById("demo").innerHTML = text;
