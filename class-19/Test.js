6a8504a09eee722ff2d3aecbe8e7908c560a788b
mdranaahmed-dev <mdranaahmed.dev>
2023-06-02 11:22:57 -0600

    numbers

---
+++ class-19/Test.js
@@ -0,0 +1,25 @@
+
+let x = 3.14;
+let y = 3;
+document.getElementById("demo").innerHTML = x + "<br>" + y;
+
+let t = 40;
+let f = 30;
+
+document.getElementById("demo2").innerHTML = t + f;
+
+let b = 123e5;
+let c = 123e-5;
+
+console.log(b + c);
+
+document.getElementById("demo3").innerHTML = b + "<br>" + c;
+
+let d = 999999999999999;
+let h = 9999999999999999;
+document.getElementById("demo4").innerHTML = d + "<br>" + h;
+
+console.log(d + h);
+console.log(x * y);
+console.log(t / f)
+
