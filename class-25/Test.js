85e40479c3de3f02e68861e04bb888b899140987
mdranaahmed-dev <mdranaahmed.dev>
2023-06-09 09:06:30 -0600

    array const

---
+++ class-25/Test.js
@@ -0,0 +1,23 @@
+const car = ["Honda", "BMW", "Hero", "Amwe"];
+document.write(car + "<br>");
+
+car[0]= "I Phon";
+document.write(car + "<br>");
+
+{
+    const car = ["Cng", "Van", "Auto"]
+    document.write(car + "<br>");
+}
+
+var nam = ["Rana", "Sabbir", "Shahabuddin"];
+document.write(nam + "<br>");
+
+var nam = ["KASWE", "SUMON", "SOHEL"];
+document.write(nam + "<br>");
+
+const join = ["HLLO", "HI", "HOW", "FINE"];
+document.getElementById("demo").innerHTML = join.join(" * ");
+
+const rana = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo2").innerHTML = rana.join(" * ");
+console.log(rana.join(" * "))
