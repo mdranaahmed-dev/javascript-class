ffeb717e54be65c593d6f5ad0f0e53690d874882
mdranaahmed-dev <mdranaahmed.dev>
2023-06-16 08:14:03 -0600

    date formets

---
+++ class-27/Test.js
@@ -0,0 +1,11 @@
+var v = new Date ();
+document.write(v + "<br>");
+
+var d = new Date ("2023-06-16");
+document.write(d + "<br>");
+
+var d = new Date("2023-7");
+document.write(d +"<br>");
+
+const f = new Date("2023");
+document.write(f + "<br>");
