5b669c5b3541e99770e818ef9429c5bfb9ffde25
mdranaahmed-dev <mdranaahmed.dev>
2023-07-17 09:59:29 -0600

    math object

---
+++ class-30/Test.js
@@ -0,0 +1,18 @@
+var d = new Date();
+d.setFullYear(2023, 6, 18); 
+document.write(d + "<br>");
+
+var d = new Date();
+d.setMonth(11);
+document.write(d + "<br>");
+
+var d = new Date();
+d.setDate(15);
+document.write(d + "<br>");
+
+const b = new Date();
+d.setHours(22);
+document.write(b + "<br>")
+
+var k = Math.floor(Math.random() * 10)() 
+document.write(k);
