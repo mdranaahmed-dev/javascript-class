5fc2b2b344bad772f23e8d4570f5919e12419054
mdranaahmed-dev <mdranaahmed.dev>
2023-07-22 08:58:22 -0600

    booleans

---
+++ class-32/Test.js
@@ -0,0 +1,20 @@
+let x = false;         // x is a Boolean
+let y = new Boolean(false);  // y is an object
+document.getElementById("demo").innerHTML = (x===y);
+
+
+let s = 10 / "Hello";
+document.getElementById("demo").innerHTML = Boolean(s);
+
+const text = ["ALRISLAIERFVNEIUSEIUG9"];
+console.log(text.sort());
+
+const student  = ["Akash", "Niloy","Rana","Abir", "Mihad", "Shahabuddin",];
+console.log(student.sort());
+
+document.getElementById("demo").innerHTML =
+Math.floor(Math.random() * 10);
+document.getElementById("demo2").innerHTML =
+Math.floor(Math.random() * 100);
+document.getElementById("demo3").innerHTML =
+Math.floor(Math.random() * 10) + 1;
