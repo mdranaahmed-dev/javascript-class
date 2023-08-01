2730c27bfdacfc6c4e10aa1f5aa4e58ce8377220
mdranaahmed-dev <mdranaahmed.dev>
2023-08-01 08:29:27 -0600

    while loop

---
+++ class-38/Test.js
@@ -0,0 +1,22 @@
+// for loop 
+var i;
+for(i=0; i<3; i=i+1) {
+ document.write(i+"Hello Bangladesh <br>") 
+}
+
+//while loop 
+
+var i =0;
+while(i<5) {
+  document.write(i+"My name is <br>")
+  i=i+1;
+}
+
+//do while loopo 
+
+var i = 0;
+
+do{
+  document.write(i+"hell<br>")
+  i=i+1
+}while (i<6)
