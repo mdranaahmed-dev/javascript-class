1a1e0b0fef366fe21c81c8ea9b52bd54a42e21ca
mdranaahmed-dev <mdranaahmed.dev>
2023-08-18 07:43:17 -0600

    typeof converson

---
+++ class-44/index.js
@@ -0,0 +1,16 @@
+document.getElementById("demo").innerHTML =
+Number("3.14") + "<br>" +
+Number(Math.PI) + "<br>" +
+Number("    ") + "<br>" +
+Number("") + "<br>" +
+Number("99 88") + "<br>" +
+Number("John") + "<br>";
+
+
+document.getElementById("demo2").innerHTML =
+(5 + null) + "<br>"  +
+("5" + null) + "<br>" +
+("5" + 2) + "<br>" +
+("5" - 2) + "<br>" +
+("5" * "2") + "<br>" +
+("5" / "2") + "<br>"
