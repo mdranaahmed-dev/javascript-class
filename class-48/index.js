8672d07ca8c83e5863e5c49b613b41743fe19898
mdranaahmed-dev <mdranaahmed.dev>
2023-08-30 10:38:41 -0600

    scope

---
+++ class-48/index.js
@@ -0,0 +1,20 @@
+{
+  var x = 4;
+}
+console.log(x);
+
+
+{
+  let z = 9;
+  console.log(z);
+}
+
+
+function exampleFunction() {
+  var x = "Hello Bangladesh"; 
+  console.log(x);
+}
+
+exampleFunction();
+
+
