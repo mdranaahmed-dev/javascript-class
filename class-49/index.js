ecdcd10ba298e6d55d8942835443811ace670bbd
mdranaahmed-dev <mdranaahmed.dev>
2023-08-31 09:48:02 -0600

    hosting

---
+++ class-49/index.js
@@ -0,0 +1,13 @@
+var x = 5; 
+var y = 7; 
+
+elem = document.getElementById("demo"); 
+elem.innerHTML = x + " " + y; 
+
+var x = 5;
+var y; 
+
+elem = document.getElementById("demo1");
+elem.innerHTML = x + " " + y; 
+
+y = 7;
