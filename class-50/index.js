27dcc7f6147702051ccd30530a7c6a1f45c34546
mdranaahmed-dev <mdranaahmed.dev>
2023-09-01 08:46:25 -0600

    strick mode

---
+++ class-50/index.js
@@ -0,0 +1,10 @@
+//Condison
+let name = prompt(`Enter Your name`);
+let age = prompt(`Enter Your Age`);
+
+if(age >20) {
+    console.log(`Hi ${name}, You are a adult`);
+}else {
+    console.log(`Hi ${name}, You are a not adult`);
+}
+
