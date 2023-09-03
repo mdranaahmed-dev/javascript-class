85b23f3b83bbb23ba3f2cad538b2fea94d4d5e61
mdranaahmed-dev <mdranaahmed.dev>
2023-09-03 08:42:06 -0600

    this keyword

---
+++ class-51/index.js
@@ -0,0 +1,24 @@
+// Create an object:
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+  
+  // Display data from the object:
+  document.getElementById("demo").innerHTML = person.fullName();
+
+  let hello = "";
+
+hello = function() {
+  document.getElementById("demo").innerHTML += this;
+}
+
+//The window object calls the function:
+window.addEventListener("load", hello);
+
+//A button object calls the function:
+document.getElementById("btn").addEventListener("click", hello);
