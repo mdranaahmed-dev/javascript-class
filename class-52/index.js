0c3d9756045cb6c9c3971a81c201bc2b5524863e
mdranaahmed-dev <mdranaahmed.dev>
2023-09-04 09:39:31 -0600

    arrow function

---
+++ class-52/index.js
@@ -0,0 +1,12 @@
+
+let hello = "";
+
+hello = () => {
+  document.getElementById("demo").innerHTML += this;
+}
+
+//The window object calls the function:
+window.addEventListener("load", hello);
+
+//A button object calls the function:
+document.getElementById("btn").addEventListener("click", hello);
